51bde37fc47f81ea5720eef524928ebc9ebdf4df
dariusfotor <darius@ezerelis.lt>
2025-12-05 12:19:06 +0200

    proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
-export default withAuth(async function middleware() {}, {
+export default withAuth(async function proxy() {}, {
   // Middleware still runs on all routes, but doesn't protect the blog route
   publicPaths: ["/"],
 });
