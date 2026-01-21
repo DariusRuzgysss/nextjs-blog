2b1b9fe6e7d6f0601a8eaa7d8dc2058b8fe4c1cc
dariusfotor <darius@ezerelis.lt>
2026-01-21 15:57:54 +0200

    xl width

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({
           <QueryProvider>
             <ProgressProvider>
               <NextIntlClientProvider messages={messages} locale={locale}>
-                <div className="min-h-screen flex flex-col py-4 px-4 lg:px-16 font-(family-name:--font-roboto)">
+                <div className="min-h-screen max-w-7xl mx-auto flex flex-col py-4 px-4 lg:px-16 font-(family-name:--font-roboto)">
                   <Navbar />
                   <main className="flex-1">{children}</main>
                 </div>
